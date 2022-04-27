ce40a380b9e22aae8a518ddc7d8ccebdceb7f8e9
user <sample@sample.com>
2022-04-27 10:10:07 -0800

    commitfive

--- club/views.py
+++ club/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
+from .models import Meeting, MeetingMinutes, Event, Resource
 
 # Create your views here.
 def index(request):
     return render(request, 'club/index.html')
+
+def gettypes(request):
+    type_list=Resource.objects.all()
+    return render(request, 'club/resource.html',{'type_list': type_list})
     
