d2d5d0502aa4a844eebdbf18ad4de638f4ac6f30
user <sample@sample.com>
2022-05-04 09:35:04 -0800

    detail

--- club/urls.py
+++ club/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
   path('', views.index, name='index'),
   path('gettypes/', views.gettypes, name='types'),
+  path('productDetail/<int:id>', views.productDetail, name='details'),
 ]
--- club/views.py
+++ club/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Meeting, MeetingMinutes, Event, Resource
-
+from django.urls import reverse_lazy
 # Create your views here.
 def index(request):
     return render(request, 'club/index.html')
@@ -8,4 +8,8 @@ def index(request):
 def gettypes(request):
     type_list=Resource.objects.all()
     return render(request, 'club/resource.html',{'type_list': type_list})
-    
+
+def productDetail(request, id): 
+    product=get_object_or_404(Resource, pk=id)
+    return render(request, 'club/productdetail.html',{'product' : product})
+
